5befa6101c43155b62c4b2ffc0f8f8d34e290409
fcucsa <lawyerfcucsa@gmail.com>
2025-09-22 00:40:25 +0300

    Change players list

--- src/data/players.js
+++ src/data/players.js
@@ -1,244 +1,258 @@
-const players = [
+[
   {
     id: 1,
+    lastName: "Амілаханов",
+    name: "Тимур Сергійович",
+    position: "Півзахисник",
+    birthDate: "21.09.1994",
+    photo: "images/players/Amilahanov.jpg"
+  },
+  {
+    id: 2,
+    lastName: "Балаба",
+    name: "Назар Андрійович",
+    position: "Захисник",
+    birthDate: "10.05.2005",
+    photo: "images/players/Balaba.jpg"
+  },
+  {
+    id: 3,
     lastName: "Баранцов",
     name: "Кирило Сергійович",
     position: "Воротар",
     birthDate: "19.09.2003",
-    photo: "images/players/Barancow_K.jpg",
-  },
-  {
-    id: 2,
-    lastName: "Гехейру Рібейру Дос Сантос",
-    name: "Венді",
-    position: "Нападник",
-    birthDate: "30.03.1998",
-    photo: "images/players/Geheiru_R.jpg",
-  },
-  {
-    id: 3,
-    lastName: "Гонсалвеш дос Сантос",
-    name: "Матеус",
-    position: "Нападник",
-    birthDate: "18.03.2003",
-    photo: "images/players/Gonsalvesh_M.jpg",
+    photo: "images/players/Barantsov.jpg"
   },
   {
     id: 4,
-    lastName: "Гоуларт",
-    name: "Габріель",
+    lastName: "Васін",
+    name: "Максим Вікторович",
     position: "Півзахисник",
-    birthDate: "10.03.2002",
-    photo: "images/players/Goulart_G.jpg",
+    birthDate: "26.05.2008",
+    photo: "images/players/Vasin.jpg"
   },
   {
     id: 5,
+    lastName: "Гонсалвеш дос Сантос",
+    name: "Матеус",
+    position: "Півзахисник",
+    birthDate: "18.03.2003",
+    photo: "images/players/Gonsalvesh_dos_Santos.jpg"
+  },
+  {
+    id: 6,
+    lastName: "Де Жесус Ліма",
+    name: "Бруну",
+    position: "Нападник",
+    birthDate: "07.04.2004",
+    photo: "images/players/De_Jesus_Lima.jpg"
+  },
+  {
+    id: 7,
+    lastName: "Де Соуза Насіменто",
+    name: "Гільерме",
+    position: "Захисник",
+    birthDate: "03.09.2003",
+    photo: "images/players/De_Souza_Nasimento.jpg"
+  },
+  {
+    id: 8,
     lastName: "Де Соуза Фрія Філу",
     name: "Ромаріу",
     position: "Півзахисник",
     birthDate: "20.09.1993",
-    photo: "images/players/Romariu.jpg",
+    photo: "images/players/De_Souza_Fria_Filu.jpg"
   },
   {
-    id: 6,
-    lastName: "Дебелко",
-    name: "Роман Миколайович",
-    position: "Нападник",
-    birthDate: "08.08.1993",
-    photo: "images/players/Debelko_R.jpg",
+    id: 9,
+    lastName: "Дзюбук",
+    name: "Ілля Русланович",
+    position: "Півзахисник",
+    birthDate: "03.05.2008",
+    photo: "images/players/Dzybuk.jpg"
   },
   {
-    id: 7,
-    lastName: "Дедяєв",
-    name: "Андрій Юрійович",
-    position: "Захисник",
-    birthDate: "09.11.1994",
-    photo: "images/players/Dedyaev_A.jpg",
-  },
-  {
-    id: 8,
+    id: 10,
     lastName: "Євдокимов",
     name: "Антон Вікторович",
     position: "Півзахисник",
     birthDate: "07.12.1995",
-    photo: "images/players/Evdokimov_A.jpg",
-  },
-  {
-    id: 9,
-    lastName: "Зозуля",
-    name: "Олексій Володимирович",
-    position: "Захисник",
-    birthDate: "15.04.1992",
-    photo: "images/players/Zozulya_O.jpg",
-  },
-  {
-    id: 10,
-    lastName: "Казаков",
-    name: "Максим Ігорович",
-    position: "Півзахисник",
-    birthDate: "06.02.1996",
-    photo: "images/players/Kazakov_M.jpg",
+    photo: "images/players/Evdokimov.jpg"
   },
   {
     id: 11,
+    lastName: "Жданович",
+    name: "Олексій Сергійович",
+    position: "Захисник",
+    birthDate: "27.09.2003",
+    photo: "images/players/Zhdanovich.jpg"
+  },
+  {
+    id: 12,
+    lastName: "Жеребецький",
+    name: "Матвій Станіславович",
+    position: "Півзахисник",
+    birthDate: "15.06.2008",
+    photo: "images/players/Zherebetskiy.jpg"
+  },
+  {
+    id: 13,
+    lastName: "Задніпрянець",
+    name: "Богдан Дмитрович",
+    position: "Півзахисник",
+    birthDate: "25.06.2003",
+    photo: "images/players/Zadnimpryanets.jpg"
+  },
+  {
+    id: 14,
+    lastName: "Західний",
+    name: "Павло Григорович",
+    position: "Захисник",
+    birthDate: "10.07.2003",
+    photo: "images/players/Zahidniy.jpg"
+  },
+  {
+    id: 15,
     lastName: "Кастро Гонсалез",
     name: "Пабло Рамон",
     position: "Півзахисник",
     birthDate: "07.02.2001",
-    photo: "images/players/Kastro_P.jpg",
-  },
-  {
-    id: 12,
-    lastName: "Кіреєв",
-    name: "Андрій Петрович",
-    position: "Півзахисник",
-    birthDate: "27.09.2001",
-    photo: "images/players/Kireev_A.jpg",
-  },
-  {
-    id: 13,
-    lastName: "Козиренко",
-    name: "Юрій Сергійович",
-    position: "Півзахисник",
-    birthDate: "27.11.1999",
-    photo: "images/players/Kozirenko_Y.jpg",
-  },
-  {
-    id: 14,
-    lastName: "Кононов",
-    name: "Микита Валентинович",
-    position: "Захисник",
-    birthDate: "22.01.2003",
-    photo: "images/players/Konov_M.jpg",
-  },
-  {
-    id: 15,
-    lastName: "Леснік",
-    name: "Сергій Володимирович",
-    position: "Нападник",
-    birthDate: "27.10.1980",
-    photo: "images/players/Lesnik_S.jpg",
+    photo: "images/players/Castro_Gonsalez.jpg"
   },
   {
     id: 16,
-    lastName: "Лук’янчук",
-    name: "Павло Олегович",
+    lastName: "Кіреєв",
+    name: "Андрій Петрович",
     position: "Захисник",
-    birthDate: "19.05.1996",
-    photo: "images/players/Lukyanchuk_P.jpg",
+    birthDate: "27.09.2001",
+    photo: "images/players/Kiriev.jpg"
   },
   {
     id: 17,
-    lastName: "Мельник",
-    name: "Олександр Сергійович",
-    position: "Півзахисник",
-    birthDate: "10.02.2000",
-    photo: "images/players/Melnik_O.jpg",
+    lastName: "Ковальов",
+    name: "Артем Андрійович",
+    position: "Воротар",
+    birthDate: "05.02.2008",
+    photo: "images/players/Kovalyov.jpg"
   },
   {
     id: 18,
+    lastName: "Лебедєв",
+    name: "Сергій Віталійович",
+    position: "Півзахисник",
+    birthDate: "30.08.2003",
+    photo: "images/players/Lebedyev.jpg"
+  },
+  {
+    id: 19,
+    lastName: "Мендес Ріела",
+    name: "Ікару",
+    position: "Нападник",
+    birthDate: "13.11.2005",
+    photo: "images/players/Mendes_Riela.jpg"
+  },
+  {
+    id: 20,
     lastName: "Монтейру Да Сілва",
     name: "Педро Акасіу",
     position: "Півзахисник",
     birthDate: "28.06.2002",
-    photo: "images/players/Monteiru_P.jpg",
-  },
-  {
-    id: 19,
-    lastName: "Селін",
-    name: "Євген Сергійович",
-    position: "Захисник",
-    birthDate: "09.05.1988",
-    photo: "images/players/Selin_E.jpg",
-  },
-  {
-    id: 20,
-    lastName: "Синьогуб",
-    name: "Олег Анатолійович",
-    position: "Захисник",
-    birthDate: "23.04.1989",
-    photo: "images/players/Sinyogub_O.jpg",
+    photo: "images/players/Monteyru_Da_Silva.jpg"
   },
   {
     id: 21,
-    lastName: "Ситніков",
-    name: "Микита Олегович",
-    position: "Захисник",
-    birthDate: "23.06.2004",
-    photo: "images/players/Sitnikov_M.jpg",
+    lastName: "Петрук",
+    name: "Нікіта Сергійович",
+    position: "Півзахисник",
+    birthDate: "08.06.2003",
+    photo: "images/players/Petruk.jpg"
   },
   {
     id: 22,
-    lastName: "Собослой",
-    name: "В’ячеслав Романович",
-    position: "Півзахисник",
-    birthDate: "02.06.2008",
-    photo: "images/players/Sobosloy_V.jpg",
+    lastName: "Поправка",
+    name: "Єгор Юрійович",
+    position: "Захисник",
+    birthDate: "12.02.2004",
+    photo: "images/players/Popravka.jpg"
   },
   {
     id: 23,
-    lastName: "Співаков",
-    name: "Андрій Сергійович",
-    position: "Півзахисник",
-    birthDate: "15.05.1995",
-    photo: "images/players/Spivakov_A.jpg",
+    lastName: "Постемський",
+    name: "Олександр Віталійович",
+    position: "Воротар",
+    birthDate: "10.03.2006",
+    photo: "images/players/Postemskiy.jpg"
   },
   {
     id: 24,
+    lastName: "Сабієв",
+    name: "Дмитро Олександрович",
+    position: "Захисник",
+    birthDate: "05.07.2004",
+    photo: "images/players/Sabiev.jpg"
+  },
+  {
+    id: 25,
+    lastName: "Сидоренко",
+    name: "Данило Павлович",
+    position: "Захисник",
+    birthDate: "13.01.2003",
+    photo: "images/players/Sidorenko.jpg"
+  },
+  {
+    id: 26,
+    lastName: "Синьогуб",
+    name: "Олег Анатолійович",
+    position: "Захисник",
+    birthDate: "19.04.1989",
+    photo: "images/players/Sinyogub.jpg"
+  },
+  {
+    id: 27,
+    lastName: "Ситников",
+    name: "Микита Олегович",
+    position: "Нападник",
+    birthDate: "23.06.2004",
+    photo: "images/players/Sitnikov.jpg"
+  },
+  {
+    id: 28,
+    lastName: "Сілва Сантос Дос Сантос",
+    name: "Венделл",
+    position: "Нападник",
+    birthDate: "14.02.2004",
+    photo: "images/players/Silva_Santos_Dos_Santos.jpg"
+  },
+  {
+    id: 29,
     lastName: "Сьєбен Пагліаріні",
     name: "Матеус",
     position: "Півзахисник",
     birthDate: "17.04.2001",
-    photo: "images/players/Syeben_M.jpg",
+    photo: "images/players/Syeben_Pagliarini.jpg"
   },
   {
-    id: 25,
-    lastName: "Татарков",
-    name: "Микита Станіславович",
-    position: "Нападник",
-    birthDate: "04.01.1995",
-    photo: "images/players/Tatarkov_M.jpg",
-  },
-  {
-    id: 26,
-    lastName: "Федотов",
-    name: "Нікіта Ігорович",
-    position: "Воротар",
-    birthDate: "27.06.2000",
-    photo: "images/players/Fedotov_N.jpg",
-  },
-  {
-    id: 27,
+    id: 30,
     lastName: "Харченко",
     name: "Матвій Анатолійович",
     position: "Півзахисник",
     birthDate: "18.10.2002",
-    photo: "images/players/Harchenko_M.jpg",
+    photo: "images/players/Harchenko.jpg"
   },
   {
-    id: 28,
-    lastName: "Чеботарьов",
-    name: "Віталій Віталійович",
-    position: "Воротар",
-    birthDate: "07.02.1996",
-    photo: "images/players/Chebotaryov_V.jpg",
+    id: 31,
+    lastName: "Ховайко",
+    name: "Кирило Юрійович",
+    position: "Півзахисник",
+    birthDate: "12.02.2004",
+    photo: "images/players/Hovayko.jpg"
   },
-  {
-    id: 29,
+   {
+    id: 32,
     lastName: "Ювхимець",
     name: "Роман Володимирович",
     position: "Півзахисник",
     birthDate: "16.03.2003",
-    photo: "images/players/Yuvhimec_R.jpg",
-  },
-  {
-    id: 30,
-    lastName: "Юзефович",
-    name: "Владислав Андрійович",
-    position: "Захисник",
-    birthDate: "11.01.1994",
-    photo: "images/players/Yuzefovich_V.jpg",
-  },
-];
-
-export default players;
+    photo: "images/players/Yuvhimetc.jpg"
+  }
+]
